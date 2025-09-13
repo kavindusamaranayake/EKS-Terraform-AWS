a43dbfe76e09f9defe19e55b9cab1e246c8c9ed8
kavindusamaranayake <k.samaranayake0026@gmail.com>
2025-09-13 17:05:35 +0530

    eks-terraform

--- main.tf
+++ main.tf
@@ -103,9 +103,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = aws_subnet.eks_subnet[*].id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 2
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
   }
 
   instance_types = ["t2.small"]
